b08f28117494e1df137349553bb3407b46adbb6f
Miguel Triana <Miguel Triana>
2023-08-09 21:32:42 -0500

    HTTP Methods en express

--- index.js
+++ index.js
@@ -9,25 +9,47 @@
 // server.listen(3000)
 // console.log(`Server on port ${3000}`)
 
+// app.get('/', (req, res) => {
+//     res.send('Hello world')
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send('About')
+// })
+
+// app.get('/weather', (req, res) => {
+//     res.send('The current weather is Nice')
+// })
+
+// app.use((req, res) => {
+//     res.status(404).send('No se encontró tu página')
+// })
+
 const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello world')
+app.get('/products', (req, res) => {
+    res.send('lista de productos')
 })
 
-app.get('/about', (req, res) => {
-    res.send('About')
+app.post('/products', (req, res) => {
+    res.send('creando productos')
 })
 
-app.get('/weather', (req, res) => {
-    res.send('The current weather is Nice')
+app.put('/products', (req, res) => {
+    res.send('actualizando un producto')
 })
 
-app.use((req, res) => {
-    res.status(404).send('No se encontró tu página')
+app.delete('/products', (req, res) => {
+    res.send('eliminando un producto')
 })
+
+app.patch('/products', (req, res) => {
+    res.send('actualizando una parte del producto')
+})
+
+
 
 app.listen(3000)
 console.log(`Server on port ${3000}`)
